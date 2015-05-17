cbe337d7570023ab4d0be7d74a86ca366178bdff
Nick Giancola <nick@gophilosophie.com>
2015-05-17 10:03:05 -0400

    Allow for input of room code while speaking

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ post '/incoming' do
   content_type 'text/xml'
 
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello. You\'ve reached Philosophie. Please enter your room number ' \
-          'followed by the pound key.',
-          voice: 'woman'
-
-    r.Gather action: '/conferences'
+    r.Gather action: '/conferences' do
+      r.Say 'Hello. You\'ve reached Philosophie. Please enter your room number ' \
+            'followed by the pound key.',
+            voice: 'woman'
+    end
   end.text
 end
 
